13664d22e5735b642850515d5447064f787780a7
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2013-04-20 18:02:06 +0000

    [minor] More executable low equivalences.

--- ifc-machine/pico/tmu/amach_ni/AbstractObsEquivFun.v
+++ ifc-machine/pico/tmu/amach_ni/AbstractObsEquivFun.v
@@ -4,9 +4,12 @@ Require Import Abstract.
 Require Import ZArith.
 Require Import AbstractObsEquiv.
 
+Require Import Coq.Bool.Bool.
+
 Set Implicit Arguments.
 
 Open Scope bool_scope.
+Open Scope list_scope.
 
 Section ObsEquivFun.
 
@@ -22,7 +25,7 @@ Proof.
   unfold lab_eqB.
   split; intros H.
   - rewrite Bool.andb_true_iff in H.
-    apply flows_antisymm; tauto.
+    apply flows_antisymm; intuition.
   - subst. rewrite flows_refl. reflexivity.
 Qed.
 
@@ -66,6 +69,148 @@ Proof.
     constructor; trivial.
   - inv H'; try congruence.
     rewrite Bool.negb_true_iff. trivial.
+Qed.
+
+Definition low_equiv_stkeltF (o : Label)
+           (e1 e2 : @StkElmt Label) : bool :=
+  match e1, e2 with
+    | AData a1, AData a2 => low_equiv_atomF o a1 a2
+    | ARet a1 b1, ARet a2 b2 =>
+      low_equiv_atomF o a1 a2 && eqb b1 b2
+    | _, _ => false
+  end.
+
+Lemma low_equiv_stkeltF_correct :
+  forall o e1 e2, low_equiv_stkeltF o e1 e2 = true <->
+                  low_equiv_stkelt o e1 e2.
+Proof.
+  intros o [a1 | a1 []] [a2 | a2 []]; simpl; split;
+  intros H; inv H; try constructor;
+  try rewrite Bool.andb_true_iff in *;
+  try rewrite low_equiv_atomF_correct in *;
+  intuition.
+Qed.
+
+Definition instr_eqB (i1 i2 : Instr) : bool :=
+  match i1, i2 with
+  | Noop, Noop => true
+  | Add, Add => true
+  | Sub, Sub => true
+  | Push z1, Push z2 => if Z.eq_dec z1 z2 then true else false
+  | Load, Load => true
+  | Store, Store => true
+  | Jump, Jump => true
+  | BranchNZ z1, BranchNZ z2 => if Z.eq_dec z1 z2 then true else false
+  | Call n1 b1, Call n2 b2 => beq_nat n1 n2 && eqb b1 b2
+  | Ret, Ret => true
+  | VRet, VRet => true
+  | Halt, Halt => true
+  | _, _ => false
+  end.
+
+Lemma instr_eqB_eq : forall i1 i2, instr_eqB i1 i2 = true <-> i1 = i2.
+Proof.
+  intros [] []; simpl; split; try congruence;
+  repeat match goal with
+           | |- context[Z.eq_dec ?z1 ?z2] =>
+             destruct (Z.eq_dec z1 z2); congruence
+         end;
+  intros;
+  rewrite Bool.andb_true_iff in *;
+  rewrite beq_nat_true_iff in *;
+  rewrite eqb_true_iff in *; intuition congruence.
+Qed.
+
+Lemma low_equiv_instr_eq :
+  forall (o : Label) i1 i2, low_equiv_instr o i1 i2 <-> i1 = i2.
+Proof.
+  intros o [] []; simpl; split;
+  intros H; inv H; constructor.
+Qed.
+
+Lemma instr_eqB_low_equiv :
+  forall (o : Label) i1 i2, instr_eqB i1 i2 = true <-> low_equiv_instr o i1 i2.
+Proof.
+  intros.
+  rewrite low_equiv_instr_eq.
+  apply instr_eqB_eq.
+Qed.
+
+Fixpoint low_equiv_listF {A} (low_equiv_aF : A -> A -> bool) (l1 l2 : list A) :=
+  match l1, l2 with
+    | nil, nil => true
+    | a1 :: l1', a2 :: l2' =>
+      low_equiv_aF a1 a2 && low_equiv_listF low_equiv_aF l1' l2'
+    | _, _ => false
+  end.
+
+Lemma low_equiv_listF_correct {A} :
+  forall (low_equiv_a : A -> A -> Prop)
+         (low_equiv_aF : A -> A -> bool)
+         (Hleq : forall a1 a2, low_equiv_aF a1 a2 = true <-> low_equiv_a a1 a2)
+         (l1 l2 : list A),
+    low_equiv_listF low_equiv_aF l1 l2 = true <->
+    low_equiv_list low_equiv_a l1 l2.
+Proof.
+  intros.
+  generalize dependent l2;
+  induction l1 as [|a1 l1' IH]; destruct l2 as [|a2 l2'];
+  simpl; split; try congruence;
+  intros H;
+  try match goal with
+        | H : low_equiv_list _ _ _ |- _ => inv H
+      end;
+  try rewrite Bool.andb_true_iff in *;
+  try constructor; firstorder.
+Qed.
+
+Definition low_equiv_full_stateF (o : Label)
+           (a1 a2 : @AS Label) : bool :=
+  match a1, a2 with
+    | AState m1 i1 s1 (pc1, l1),
+      AState m2 i2 s2 (pc2, l2) =>
+      if l1 <: o then
+        atom_eqB (pc1, l1) (pc2, l2) &&
+        low_equiv_listF (low_equiv_atomF o) m1 m2 &&
+        low_equiv_listF (low_equiv_stkeltF o) s1 s2 &&
+        low_equiv_listF instr_eqB i1 i2
+      else
+        negb (l2 <: o) &&
+        low_equiv_listF (low_equiv_atomF o) m1 m2 &&
+        low_equiv_listF (low_equiv_stkeltF o)
+                        (below_lret o s1) (below_lret o s2) &&
+        low_equiv_listF instr_eqB i1 i2
+  end.
+
+Lemma low_equiv_full_stateF_correct :
+  forall o a1 a2,
+    low_equiv_full_stateF o a1 a2 = true <->
+    low_equiv_full_state o a1 a2.
+Proof.
+  intros o [m1 i1 s1 [pc1 l1]] [m2 i2 s2 [pc2 l2]]; simpl;
+  destruct (l1 <: o) eqn:Hl1;
+  split;
+  intros H;
+  repeat rewrite Bool.andb_true_iff in *;
+  intuition;
+  try rewrite atom_eqB_eq in *;
+  repeat match goal with
+           | H : (_, _) = (_, _) |- _ => inv H
+           | H : low_equiv_full_state _ _ _ |- _ => inv H
+         end;
+  try rewrite negb_true_iff in *;
+  try congruence;
+  try solve [
+        rewrite low_equiv_listF_correct;
+        eauto using low_equiv_atomF_correct,
+                    low_equiv_stkeltF_correct,
+                    instr_eqB_low_equiv
+      ];
+
+  econstructor (auto; solve [rewrite <- low_equiv_listF_correct;
+    eauto using instr_eqB_low_equiv, low_equiv_stkeltF_correct,
+    low_equiv_atomF_correct]).
+
 Qed.
 
 End ObsEquivFun.
