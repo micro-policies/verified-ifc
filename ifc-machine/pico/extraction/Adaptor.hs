61ac04bef19b9817e5beb93c11157747d90438e2
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2013-04-21 18:01:53 +0000

    [minor] Merge old Driver in new file.

--- ifc-machine/pico/extraction/Adaptor.hs
+++ ifc-machine/pico/extraction/Adaptor.hs
@@ -1,12 +1,20 @@
-import qualified Lattices
-import qualified LatticeHL
+import Abstract
+import qualified Lattices as L
+import LatticeHL
 
 class JoinSemiLattice l where
   bot :: l
   join :: l -> l -> l
   flows :: l -> l -> Bool
 
-instance JoinSemiLattice LatticeHL.Lab where
-  bot = Lattices.bot LatticeHL.coq_HL
-  join = Lattices.join LatticeHL.coq_HL
-  flows = Lattices.flows LatticeHL.coq_HL
+instance JoinSemiLattice Lab where
+  bot = L.bot coq_HL
+  join = L.join coq_HL
+  flows = L.flows coq_HL
+
+instance Show Lab where
+   show L = "L"
+   show H = "H"
+
+instance (Show a) => Show (AS a) where
+    show s = "AS"
