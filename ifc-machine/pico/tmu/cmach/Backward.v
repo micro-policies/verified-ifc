357678b78f44ff42496cb8ac8b989b47f35319b7
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2013-05-29 14:30:06 +0000

    [minor] Some progress on cache hit simulation.

--- ifc-machine/pico/tmu/cmach/Backward.v
+++ ifc-machine/pico/tmu/cmach/Backward.v
@@ -1,9 +1,21 @@
 Require Import List.
+Require Import ZArith.
 
 Require Import Utils.
+Require Import Lattices.
+Require Import CLattices.
+Require Import WfCLattices.
+Require Import TMUInstr.
+Require Import Abstract.
+Require Import Rules.
+Require Import QuasiAbstractMachine.
 Require Import Concrete.
 Require Import ConcreteMachineSmallStep.
+Require Import Determinism.
+Require Import Refinement.
 Require TINI.
+
+Open Scope Z_scope.
 
 Set Implicit Arguments.
 
@@ -164,5 +176,106 @@ Proof.
   rewrite cons_event_remove_silent.
   eapply cexec_step; eauto.
 Qed.
+
+Section Simulation.
+
+Context {L: Type}
+        {Latt: JoinSemiLattice L}
+        {CLatt: ConcreteLattice L}
+        {WFCLatt: WfConcreteLattice L Latt CLatt}.
+
+Definition abstract_event (ce : option CEvent) : option (@Event L) :=
+  match ce with
+    | Some (CEInt ca) => Some (EInt (atom_ZToLab ca))
+    | None => None
+  end.
+
+(* FIXME: Move this somewhere else *)
+Definition opcode_of_instr (i : Instr) : option OpCode :=
+  match i with
+    | Noop => Some OpNoop
+    | Add => Some OpAdd
+    | Sub => Some OpSub
+    | Push _ => Some OpPush
+    | Load => Some OpLoad
+    | Store => Some OpStore
+    | Jump => Some OpJump
+    | BranchNZ _ => Some OpBranchNZ
+    | Call _ _ => Some OpCall
+    | Ret => Some OpRet
+    | VRet => Some OpVRet
+    | Halt => None
+    | Output => Some OpOutput
+  end.
+
+Lemma cache_hit_simulation :
+  forall s1 s2 e s2'
+         (Hmatch : match_states s1 s2)
+         (Hs2' : priv s2' = false)
+         (Hstep : cstep s2 e s2'),
+    exists s1', step_rules s1 (abstract_event e) s1' /\
+                match_states s1' s2'.
+Proof.
+  intros.
+  inv Hmatch.
+  destruct apc as [apc apcl].
+  inv Hstep; simpl in *;
+
+  (* Invert some hypothesis *)
+  repeat match goal with
+           | H : true = false |- _ => inv H
+           | H : ?x = ?x |- _ => clear H
+           | H : match_stacks _ (_ ::: _) |- _ => inv H
+           | a : _,
+             H : (_, _) = atom_labToZ ?a |- _ =>
+             destruct a; simpl in H; inv H
+         end;
+
+  (* Reconstruct the quasi-abstract label vector *)
+  try match goal with
+    | H : cache_hit _ _ (dontCare, dontCare, dontCare) _ |- _ =>
+      pose (vls := Vector.nil L)
+    | H : cache_hit _ _ (labToZ ?l, dontCare, dontCare) _ |- _ =>
+      pose (vls := Vector.cons _ l _ (Vector.nil _))
+    | H : cache_hit _ _ (labToZ ?l1, labToZ ?l2, dontCare) _ |- _ =>
+      pose (vls := Vector.cons _ l1 _ (Vector.cons _ l2 _ (Vector.nil _)))
+    | H : cache_hit _ _ (labToZ ?l1, labToZ ?l2, labToZ ?l3) _ |- _ =>
+      pose (vls := Vector.cons _ l1 _
+                               (Vector.cons _ l2 _
+                                            (Vector.cons _ l3 _ (Vector.nil _))))
+  end;
+
+  (* Find the current opcode *)
+  match goal with
+    | H : read_m _ _ = Some ?instr |- _ =>
+      let opcode := (eval compute in (opcode_of_instr instr)) in
+      match opcode with
+        | Some ?opcode => pose (OP := opcode)
+      end
+  end;
+
+  try (exploit (@CACHE OP vls apcl _ apcl eq_refl); eauto;
+       intros H;
+       repeat match goal with
+                | H : exists t, _ |- _ =>
+                  destruct H as [t' H]
+              end;
+
+       match goal with
+         | H1 : cache_hit_read _ _ _,
+                H2 : cache_hit_read _ _ _ |- _ =>
+           let H := fresh "H" in
+           generalize (cache_hit_read_determ H1 H2); intros H
+       end);
+
+  try solve [
+        eexists; split;
+        try (econstructor (solve [compute; eauto]));
+        repeat (constructor; eauto); simpl; f_equal; intuition
+      ].
+
+Admitted.
+
+End Simulation.
 
 End Backward.
