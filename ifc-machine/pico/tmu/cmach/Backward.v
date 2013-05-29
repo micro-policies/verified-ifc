223a86e39f4f89d41d45430bdfaa75c780cbb30b
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2013-05-29 14:29:55 +0000

    [minor] Begin work on backward simulation.

---
+++ ifc-machine/pico/tmu/cmach/Backward.v
@@ -0,0 +1,161 @@
+Require Import List.
+
+Require Import Utils.
+Require Import Concrete.
+Require Import ConcreteMachineSmallStep.
+Require TINI.
+
+Set Implicit Arguments.
+
+Section Backward.
+
+Let ctrace := list (option CEvent).
+Let exec := TINI.exec cstep.
+
+Inductive kernel_run_until_user : CS -> CS -> Prop :=
+| kruu_end : forall s s',
+               priv s = true ->
+               priv s' = false ->
+               cstep s None s' ->
+               kernel_run_until_user s s'
+| kruu_step : forall s s' s'',
+                priv s = true ->
+                cstep s None s' ->
+                kernel_run_until_user s' s'' ->
+                kernel_run_until_user s s''.
+Hint Constructors kernel_run_until_user.
+
+Lemma kernel_run_until_user_l : forall s s',
+                                  kernel_run_until_user s s' ->
+                                  priv s = true.
+Proof.
+  intros. inv H; trivial.
+Qed.
+
+Lemma kernel_run_until_user_r : forall s s',
+                                  kernel_run_until_user s s' ->
+                                  priv s' = false.
+Proof.
+  intros. induction H; auto.
+Qed.
+
+Inductive kernel_run : CS -> CS -> Prop :=
+| kr_refl : forall s, priv s = true -> kernel_run s s
+| kr_step : forall s s' s'',
+              priv s = true ->
+              cstep s None s' ->
+              kernel_run s' s'' ->
+              kernel_run s s''.
+Hint Constructors kernel_run.
+
+Lemma kernel_run_l : forall s s',
+                       kernel_run s s' ->
+                       priv s = true.
+Proof.
+  intros. inv H; trivial.
+Qed.
+
+Lemma kernel_run_r : forall s s',
+                       kernel_run s s' ->
+                       priv s' = true.
+Proof.
+  intros. induction H; auto.
+Qed.
+
+Let cons_event e t : ctrace :=
+  match e with
+    | Some _ => e :: t
+    | None => t
+  end.
+
+Inductive cexec : CS -> ctrace -> CS -> Prop :=
+| ce_refl : forall s, cexec s nil s
+| ce_kernel_end : forall s s', kernel_run s s' -> cexec s nil s'
+| ce_kernel_user : forall s s' t s'',
+                     kernel_run_until_user s s' ->
+                     cexec s' t s'' ->
+                     cexec s t s''
+| ce_user_step : forall s e s' t s'',
+                   priv s = false ->
+                   cstep s e s' ->
+                   cexec s' t s'' ->
+                   cexec s (cons_event e t) s''.
+Hint Constructors cexec.
+
+Lemma cexec_step : forall s e s' t s''
+                          (Hstep : cstep s e s')
+                          (Hexec : cexec s' t s''),
+                          cexec s (cons_event e t) s''.
+Proof.
+  (* Automation disaster.... :( *)
+  intros.
+  inv Hexec; simpl;
+  destruct (priv s) eqn:Hs; eauto.
+
+  - destruct (priv s'') eqn:Hs'; eauto;
+
+    (* congruence is not working here... *)
+    inversion Hstep; subst; simpl in *;
+    repeat match goal with
+             | H : false = true |- _ =>
+               inversion H
+             | H : true = false |- _ =>
+               inversion H
+             | H : ?x = ?x |- _ => clear H
+           end; eauto.
+
+    eapply ce_kernel_user; eauto; solve [eauto].
+
+  - generalize (kernel_run_l H). intros H'.
+
+    inversion Hstep; subst; simpl in *;
+    repeat match goal with
+             | H : false = true |- _ =>
+               inversion H
+             | H : true = false |- _ =>
+               inversion H
+             | H : ?x = ?x |- _ => clear H
+           end; eauto.
+
+  - generalize (kernel_run_until_user_l H). intros H'.
+
+    inversion Hstep; subst; simpl in *;
+    repeat match goal with
+             | H : false = true |- _ =>
+               inversion H
+             | H : true = false |- _ =>
+               inversion H
+             | H : ?x = ?x |- _ => clear H
+           end; eauto.
+
+  - inversion Hstep; subst; simpl in *;
+    repeat match goal with
+             | H : false = true |- _ =>
+               inversion H
+             | H : true = false |- _ =>
+               inversion H
+             | H : ?x = ?x |- _ => clear H
+           end; eauto.
+
+    subst.
+    eapply ce_kernel_user; eauto.
+    eapply kruu_end; eauto.
+    eauto.
+
+    eauto.
+Qed.
+
+(*
+Let remove_silent (ct : ctrace) :=
+  filter (fun e => match e with Some _ => true | _ => false end) ct.
+
+Lemma exec_cexec : forall s t s',
+                     exec s t s' ->
+                     exists t',
+                       cexec s t' s' /\
+                       remove_silent t = remove_silent t'.
+Proof.
+  intros s t s' Hexec.
+  induction Hexec; eauto.
+  destruct IHHexec as [t' [H1 H2]].
+*)
