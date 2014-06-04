0f999652b757005a4cf4168739528c011bcfbbdc
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-06-04 14:55:46 +0200

    Started reorganizing verif folder; please pull asap
    Here is what I did:
    [hritcu@detained verif]$ mv ifc-machine ifc
    [hritcu@detained verif]$ mv non-deterministic-pico ifc
    [hritcu@detained verif]$ mv pico-ifc-coq ifc
    [hritcu@detained verif]$ mv sfi stock-pico/notes
    [hritcu@detained verif]$ cd stock-pico
    [hritcu@detained stock-pico]$ mv latex initial-stock-pico

--- ifc-machine/old/pico_dep/example.dir-locals.el
+++
@@ -1,9 +0,0 @@
-((coq-mode . (
-   ;; HACK: Include everything at two levels, so that relative paths
-   ;; make sense when editing a file at either level.
-   (coq-load-path . ("../lib" "../../lib"
-                     "../common" "../../common"
-                     "../tmu" "../../tmu"
-                     "../tmu/amach" "../../tmu/amach"
-                     "../tmu/cmach" "../../tmu/cmach"
-                     "../tmu/amach_ni" "../../tmu/amach_ni")))))
